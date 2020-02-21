0449a37c6755972da83e3f42ac181e1bbcea4fc4
hisamatsu yoshiyuki <hisasann25@gmail.com>
2020-02-21 17:47:35 +0900

    🥐 feat: add fish/config.fish

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+set -U FZF_LEGACY_KEYBINDINGS 0
+set -U FZF_REVERSE_ISEARCH_OPTS "--reverse --height=100%"
+
+set ANDROID_HOME $HOME/Library/Android/sdk
+set PATH $ANDROID_HOME/emulator $PATH
+set PATH $ANDROID_HOME/tools $PATH
+set PATH $ANDROID_HOME/tools/bin $PATH
+set PATH $ANDROID_HOME/platform-tools $PATH
+
+source ~/.asdf/asdf.fish
+source /usr/local/opt/asdf/asdf.fish
+source ~/.asdf/asdf.fish
+source ~/.asdf/asdf.fish
