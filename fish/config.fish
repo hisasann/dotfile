775ba763e25ae53f96e70afd2813a509d12f5fb8
hisamatsu yoshiyuki <hisasann25@gmail.com>
2020-10-22 21:20:04 +0900

    feat: add show gitstatus to fish/config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,26 @@ set PATH $ANDROID_HOME/emulator $PATH
 set PATH $ANDROID_HOME/tools $PATH
 set PATH $ANDROID_HOME/tools/bin $PATH
 set PATH $ANDROID_HOME/platform-tools $PATH
+set PATH /usr/local/opt/mongodb-community@3.6/bin $PATH
+set PATH /usr/local/Cellar/mongodb-community-shell/4.2.0/bin $PATH
+
+# via fishのプロンプトの右側をカスタマイズして、gitのブランチとstatusを表示させる - Qiita - https://qiita.com/mom0tomo/items/b593c0e98c1eea70a114
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
 
 source ~/.asdf/asdf.fish
 source /usr/local/opt/asdf/asdf.fish
