e1d15c66c560194cdab6f2090c773b540725f281
hisamatsu yoshiyuki <hisasann25@gmail.com>
2021-10-22 15:26:37 +0900

    feat(config.fish): add comment

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,16 @@
 set -U FZF_LEGACY_KEYBINDINGS 0
 set -U FZF_REVERSE_ISEARCH_OPTS "--reverse --height=100%"
 
+# Android
 set ANDROID_HOME $HOME/Library/Android/sdk
 set PATH $ANDROID_HOME/emulator $PATH
 set PATH $ANDROID_HOME/tools $PATH
 set PATH $ANDROID_HOME/tools/bin $PATH
 set PATH $ANDROID_HOME/platform-tools $PATH
+# Mongodb
 set PATH /usr/local/opt/mongodb-community@3.6/bin $PATH
 set PATH /usr/local/Cellar/mongodb-community-shell/4.2.0/bin $PATH
+# Rust
 set PATH $HOME/.cargo/bin $PATH
 
 # via fishのプロンプトの右側をカスタマイズして、gitのブランチとstatusを表示させる - Qiita - https://qiita.com/mom0tomo/items/b593c0e98c1eea70a114
